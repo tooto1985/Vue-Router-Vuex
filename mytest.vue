e764011382e78419a3e404f6e9baae14b14dceed
Tommy <tooto1985@gmail.com>
2018-06-06 12:26:28 +0800

    add vue file

---
+++ mytest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="t1">
+      {{title}}
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: 'mytest',
+  data() {
+    return {
+      title: 'mytest'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .t1 {
+    color: red;
+  }
+</style>
